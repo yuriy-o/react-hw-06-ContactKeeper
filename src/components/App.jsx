f385c11edffb0b85f63070d0cbf9488a6aa04f86
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-04-12 12:40:13 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
-import { ContactForm } from './ContactForm/ContactForm';
-import { ContactList } from './ContactList/ContactList';
-import { ContactsFilter } from './ContactsFilter/ContactsFilter';
+import { ContactForm } from "./ContactForm/ContactForm";
+import { ContactList } from "./ContactList/ContactList";
+import { ContactsFilter } from "./ContactsFilter/ContactsFilter";
 
-import { Container, H1, H2 } from './App.styled';
+import { Container, H1, H2 } from "./App.styled";
 
 export const App = () => {
   return (
     <Container>
-      <H1>Phonebook</H1>
+      <H1>ContactKeeper</H1>
       <ContactForm />
       <H2>Contacts</H2>
       <ContactsFilter />
